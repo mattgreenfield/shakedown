98b90e80b8d515bc43eed81aa57c40bcb1338f8c
Matt Greenfield <gmattgreenfield@gmail.com>
2014-08-05 22:02:23 +0100

    set up gulp to compile sass, autoprefix and minify css. watch by default

--- _src/gulpfile.js
+++ _src/gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var prefix = require('gulp-autoprefixer');
+
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -9,16 +13,13 @@ gulp.task('default', function() {
 gulp.task('sass', function () {
     gulp.src('*.scss')
         .pipe(sass())
+        .pipe(prefix(["last 1 version", "> 1%", "ie 8", "ie 7"]))
+        .pipe(minifyCSS())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('../'));
 });
+
 
 gulp.task('default', function () {
     gulp.watch('*.scss', ['sass']);
 });
-
-
-// autoprfixer
-//
-// minify
-//
-// rename to .min.css
